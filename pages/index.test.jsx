a2d788bbc0d89e4cde04c74afcb8e269b3be8887
emanuelefavero <emafaux89@gmail.com>
2023-04-16 23:24:31 +0200

    update first test

--- pages/index.test.jsx
+++ pages/index.test.jsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Home from './index'
 
 describe('Home', () => {
   it('renders a heading with the text "Hello"', () => {
-    const { getByRole } = render(<Home />)
-    const headingElement = getByRole('heading', { name: 'Hello' })
+    render(<Home />)
+    const headingElement = screen.getByRole('heading', { name: 'Hello' })
     expect(headingElement).toBeInTheDocument()
   })
 })
