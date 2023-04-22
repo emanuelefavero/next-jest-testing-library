95d6ad8b0ba5c9baafd8264aec9b4a3a9d43f066
emanuelefavero <emafaux89@gmail.com>
2023-04-22 06:46:21 +0200

    add bootstrap to summary-form

--- pages/sundaes-on-demand/summary-form.tsx
+++ pages/sundaes-on-demand/summary-form.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 export default function SummaryForm() {
   const [checked, setChecked] = useState(false)
@@ -6,24 +8,23 @@ export default function SummaryForm() {
 
   return (
     <>
-      <input
-        type='checkbox'
-        id='i-agree-to-terms-and-conditions-checkbox'
-        defaultChecked={checked}
-        onChange={() => {
-          setChecked(!checked)
-          setDisabled(!disabled)
-        }}
-      />
-      <label htmlFor='i-agree-to-terms-and-conditions-checkbox'>
-        I agree to Terms and Conditions
-      </label>
-      <button
-        disabled={disabled}
-        style={disabled ? { opacity: 0.5, cursor: 'default' } : {}}
-      >
-        Confirm order
-      </button>
+      <Form>
+        <Form.Check
+          type='switch'
+          defaultChecked={checked}
+          onChange={() => {
+            setChecked(!checked)
+            setDisabled(!disabled)
+          }}
+          // TIP: The id attribute is used to associate a label with an input element, it is needed for tests to work (it replaces the htmlFor attribute)
+          id='i-agree-to-terms-and-conditions-checkbox'
+          label='I agree to Terms and Conditions'
+        />
+
+        <Button variant='primary' disabled={disabled} type='submit'>
+          Confirm order
+        </Button>
+      </Form>
     </>
   )
 }
