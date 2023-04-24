af4d9c4e03983f80e41a84a87d665d5c9c7a763e
emanuelefavero <emafaux89@gmail.com>
2023-04-24 05:36:52 +0200

    add TODO in sundaes-on-demand/index.tsx

--- pages/sundaes-on-demand/index.tsx
+++ pages/sundaes-on-demand/index.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import Options from '@/components/Options'
 
+// TODO: Remember to start the api server (sundae-server, localhost:3030) before running this app locally
+
 export default function SundaesOnDemand() {
   return (
     <>
