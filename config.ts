25a73f440af8301c004259d3fb1ea25352dd8863
emanuelefavero <emafaux89@gmail.com>
2023-04-24 03:23:00 +0200

    update config.ts with env variables

--- config.ts
+++ config.ts
@@ -1,7 +1,10 @@
+// TODO: Change the values to your own
+const devServer = process.env.DEV_SERVER || 'http://localhost:3000'
+const devApiURL = process.env.DEV_API_URL || 'http://localhost:3030'
+const prodServer = process.env.SERVER || 'https://yourwebsite.com'
+const prodApiURL = process.env.API_URL || 'https://yourapiservice.com'
+
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
-
-export const apiURL = dev
-  ? 'http://localhost:3030'
-  : 'https://yourwebsite.com/api'
+export const server = dev ? devServer : prodServer
+export const apiURL = dev ? devApiURL : prodApiURL
